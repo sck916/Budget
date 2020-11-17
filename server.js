b3b4065bb3770712bb763cac9665a0c1be7291d3
Steve Kavuu <mrkavuu@gmail.com>
2020-11-17 14:59:10 -0600

    more

--- server.js
+++ server.js
@@ -1,76 +1,30 @@
-// require('dotenv').config();
-
-// const express = require("express");
-// const logger = require("morgan");
-// const mongoose = require("mongoose");
-// const compression = require("compression");
-
-// const PORT = 3000;
-
-// const app = express();
-
-// app.use(logger("dev"));
-
-// app.use(compression());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(express.static("public"));
-
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
-// // routes
-// app.use(require("./routes/api.js"));
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
-
 const express = require("express");
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const logger = require("morgan");
+const mongoose = require("mongoose");
 const compression = require("compression");
-const bodyParser = require('body-parser');
-const path = require('path');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session)
 
-
-// Load config
-dotenv.config({path: './config/config.env'})
-
-const connectDB = require('./config/db');
-connectDB();
+const PORT =process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
 
-// body Parser
 app.use(compression());
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// express-session middleware
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
-}))
+app.use(express.static("public"));
 
-// Static
-app.use(express.static('public'));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
-app.use(require('./routes/api.js'));
+app.use(require("./routes/api.js"));
+require('./routes/html.js')(app);
 
-const PORT = process.env.PORT || 8080
-
-
-app.listen(PORT, console.log(`Server running on ${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
